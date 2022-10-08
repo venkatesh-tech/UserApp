3ec0c86ccb0a82dac9bd7f5d83b343b09d6149a2
Venkateshe <kolapallinagavenkatesh@gmail.com>
2022-10-08 12:22:00 +0530

    Added ReadUsersServlet

---
+++ src/main/java/com/user/servlets/ReadUsersServlet.java
@@ -0,0 +1,52 @@
+package com.user.servlets;
+
+import java.io.*;
+import java.sql.*;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+
+@WebServlet("/updateServlet")
+public class ReadUsersServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private Connection connection;
+
+	public void init() { // Called only once
+
+		try {
+			System.out.println("init");
+			Class.forName("com.mysql.cj.jdbc.Driver"); // Required for tomact
+			connection = DriverManager.getConnection("jdbc:mysql://localhost/mydb", "root", "Venky1234@");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		System.out.println("doPost");
+
+		try {
+			Statement statement = connection.createStatement(); // Creation of statement
+			ResultSet resultSet = statement.executeQuery("select * from user");
+			PrintWriter out = response.getWriter();
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void destroy() { // Called only once
+
+		try {
+			System.out.println("destroy");
+			connection.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
